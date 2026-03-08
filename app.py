2604004e9138012a5ae5189c7587cd2121f7bb8e
02838-vae <vietson1901@gmail.com>
2026-03-08 09:56:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -253,7 +253,7 @@ st.components.v1.html(f"""
   <div id="logo"><img src="data:image/jpeg;base64,{logo_b64}" alt="Logo"/></div>
   <div id="content">
     <div class="btn-wrap">
-      <a class="btn" href="/partnumber" target="_self">
+      <a class="btn" href="/partnumber" target="_blank">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
         </svg>
@@ -301,4 +301,3 @@ iframe {
 }
 </style>
 """, unsafe_allow_html=True)
-
